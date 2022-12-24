69d70a43ff9e1f4ac5cda77658a82a32ca0e8e61
mirrotoday <mirrotoday>
2022-12-24 14:57:03 +0800

    ZsyMes

--- src/main/java/com/siyu/zsymes/controller/TestController.java
+++ src/main/java/com/siyu/zsymes/controller/TestController.java
@@ -1,15 +1,15 @@
 package com.siyu.zsymes.controller;
 
-import com.siyu.zsymes.config.redis.RedisRepository;
 import com.siyu.zsymes.service.TestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(value = "/login")
@@ -18,16 +18,17 @@ public class TestController {
 
     @Resource
     TestService testService;
+//    @Resource
+//    private RedisTemplate redisTemplate;
     @Resource
-    RedisRepository  redisRepository;
+    RedisTemplate<String, Object> redisTemplate1;
 
     @GetMapping(value = "/validate")
     @ApiOperation(value = "登录")
     public String Login(){
-        redisRepository.set("user","ceshi");
-        String key = redisRepository.get("user");
-        System.out.println("====================进入登录"+key);
-        return "登录";
-
+        redisTemplate1.opsForValue().set("user","张三");
+       String emp = (String)redisTemplate1.opsForValue().get("user");
+        System.out.println("====================进入登录"+emp);
+        return emp;
     }
 }
