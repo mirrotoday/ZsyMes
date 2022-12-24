9c47823b10b0c498725849985e49b056ffc05a2b
mirrotoday <mirrotoday>
2022-12-24 15:09:16 +0800

    ZsyMes

--- src/main/java/com/siyu/zsymes/controller/TestController.java
+++ src/main/java/com/siyu/zsymes/controller/TestController.java
@@ -1,33 +1,32 @@
 package com.siyu.zsymes.controller;
 
-import com.siyu.zsymes.service.TestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * @author SiYu
+ */
 @RestController
 @RequestMapping(value = "/login")
 @Api(tags = "登录")
 public class TestController {
 
     @Resource
-    TestService testService;
-//    @Resource
-//    private RedisTemplate redisTemplate;
-    @Resource
     RedisTemplate<String, Object> redisTemplate1;
 
     @GetMapping(value = "/validate")
     @ApiOperation(value = "登录")
-    public String Login(){
+    public String login(){
         redisTemplate1.opsForValue().set("user","张三");
        String emp = (String)redisTemplate1.opsForValue().get("user");
+       //设置过期策略
+        redisTemplate1.expire("user",10, TimeUnit.MINUTES);
         System.out.println("====================进入登录"+emp);
         return emp;
     }
